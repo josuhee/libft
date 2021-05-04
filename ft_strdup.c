8641514ecfe008c08d65ec32d29e694fa192a957
josuhee <suheeqwe@gmail.com>
2021-05-04 15:12:54 +0200

    strdup modi

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,21 +6,11 @@
 /*   By: sujo <sujo@student.42soul.kr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/04 14:07:18 by sujo              #+#    #+#             */
-/*   Updated: 2021/05/04 14:11:54 by sujo             ###   ########.fr       */
+/*   Updated: 2021/05/04 15:12:42 by sujo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-size_t	ft_strlen(const char *str)
-{
-	size_t idx;
-
-	idx = 0;
-	while (*str++)
-		idx++;
-	return (idx);
-}
 
 char	*ft_strdup(const char *s)
 {
