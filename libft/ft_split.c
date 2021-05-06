c61f835cb3918855d81177144d22680a17b66b61
josuhee <suheeqwe@gmail.com>
2021-05-06 20:54:37 +0200

    split

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -5,53 +5,49 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: sujo <sujo@student.42soul.kr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/05/04 15:15:52 by sujo              #+#    #+#             */
-/*   Updated: 2021/05/06 23:19:53 by sujo             ###   ########.fr       */
+/*   Created: 2021/05/06 19:29:45 by sujo              #+#    #+#             */
+/*   Updated: 2021/05/06 20:45:46 by sujo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	str_size(char const *s, char c)
+int		str_size(const char *str, char c)
 {
 	size_t	size;
 	int		flag;
 
 	size = 0;
 	flag = 1;
-	while (*s)
+	while (*str)
 	{
-		if (flag && *s != c)
+		if (flag && *str != c)
 		{
 			size++;
 			flag = 0;
 		}
-		if (*s == c)
+		if (*str == c)
 			flag = 1;
-		s++;
+		str++;
 	}
 	return (size);
 }
 
-#include <stdio.h>
-
 char	*get_str(char const *s, char c)
 {
-	char		*str;
-	size_t		idx;
-	size_t		len;
+	char		*result;
+	const char	*ptr;
+	size_t		length;
 
-	len = 0;
-	while (s[len] && s[len] != c)
-		len++;
-	str = (char *)malloc(len + 1);
-	if (!str)
+	ptr = s;
+	length = 0;
+	while (*ptr && *ptr++ != c)
+		length++;
+	result = (char *)malloc(length + 1);
+	if (!result)
 		return (0);
-	idx = 0;
-	while (idx < len)
-		str[idx] = (char)s[idx];
-	str[len] = '\0';
-	return (str);
+	ft_strlcpy(result, s, length + 1);
+	return (result);
 }
 
 char	**go_free(char **str)
@@ -66,41 +62,28 @@ char	**go_free(char **str)
 
 char	**ft_split(char const *s, char c)
 {
-	char		**result;
-	int			idx;
+	char	**result;
+	int		idx;
 
 	if (!s)
 		return (0);
-	result = (char **)malloc(sizeof(char *) * (str_size(s, c) + 1));
+	result = (char **)malloc(sizeof(char*) * (str_size(s, c) + 1));
 	if (!result)
 		return (0);
 	idx = 0;
-	while (*s)
+	while (*s != '\0')
 	{
 		if (*s != c)
 		{
-			printf("1");
 			result[idx] = get_str(s, c);
-			if (!result[idx])
+			if (!result[idx++])
 				return (go_free(result));
-			printf("%s\n", result[idx]);
-			idx++;
+			while (*s && *s != c)
+				s++;
 		}
-		if (!*s)
-			break ;
-		s++;
+		else
+			s++;
 	}
 	result[idx] = 0;
 	return (result);
-}
-
-int main(){
-	char *s = "      split       this for   me  !       ";
-
-	char **result = ft_split(s, ' ');
-
-	for (int i=0;i<5;i++)
-		printf("%s\n", result[i]);
-	printf("%s\n", result[5]);
-	return 0;
 }
